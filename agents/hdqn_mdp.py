229a7d9c18b95db88217dac0c6fb22afd74a22e7
Rao-star <ziyirao@outlook.com>
2025-05-21 00:58:46 +0800

    Add files via upload

--- agents/hdqn_mdp.py
+++ agents/hdqn_mdp.py
@@ -6,11 +6,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
-
+from minigrid.core.constants import COLOR_TO_IDX, OBJECT_TO_IDX
 from utils.replay_memory import ReplayMemory, Transition
 
 USE_CUDA = torch.cuda.is_available()
 dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+GOAL_TYPE = OBJECT_TO_IDX["goal"]
 
 class Variable(autograd.Variable):
     def __init__(self, data, *args, **kwargs):
@@ -35,7 +37,7 @@ class MetaController(nn.Module):
         return self.fc2(x)
 
 class Controller(nn.Module):
-    def __init__(self, in_features=80, out_features=3):
+    def __init__(self, in_features=79, out_features=7):
         """
         Initialize a Controller(given goal) of h-DQN for the diecreate mdp experiment
             in_features: number of features of input.
@@ -75,29 +77,58 @@ class hDQN():
             How many transitions to sample each time experience is replayed.
     """
     def __init__(self,
+                 env,
                  optimizer_spec,
-                 num_goal=7,
-                 num_action=3,
+                 num_goal=4,
+                 num_action=7,
                  replay_memory_size=10000,
                  batch_size=128):
         ###############
         # BUILD MODEL #
         ###############
+        self.env = env
         self.num_goal = num_goal
         self.num_action = num_action
         self.batch_size = batch_size
         # Construct meta-controller and controller
-        self.meta_controller = MetaController(in_features=79, out_features=7).type(dtype)
-        self.target_meta_controller = MetaController(in_features=79, out_features=7).type(dtype)
-        self.controller = Controller().type(dtype)
-        self.target_controller = Controller().type(dtype)
+        controller_input_dim = 79 + num_goal  #(75+4 direction)
+        meta_input_dim = 79  # meta-controller 只看环境状态
+        self.meta_controller = MetaController(in_features=meta_input_dim, out_features=num_goal).type(dtype)
+        self.target_meta_controller = MetaController(in_features=meta_input_dim, out_features=num_goal).type(dtype)
+        self.controller = Controller(in_features=controller_input_dim, out_features=num_action).type(dtype)
+        self.target_controller = Controller(in_features=controller_input_dim, out_features=num_action).type(dtype)
         # Construct the optimizers for meta-controller and controller
         self.meta_optimizer = optimizer_spec.constructor(self.meta_controller.parameters(), **optimizer_spec.kwargs)
         self.ctrl_optimizer = optimizer_spec.constructor(self.controller.parameters(), **optimizer_spec.kwargs)
         # Construct the replay memory for meta-controller and controller
         self.meta_replay_memory = ReplayMemory(replay_memory_size)
         self.ctrl_replay_memory = ReplayMemory(replay_memory_size)
-        self.possible_goals = [6]
+        self.possible_goals = [0, 1, 2, 3]
+
+    def get_agent_position(self):
+        return self.env.unwrapped.agent_pos
+
+    def near_key(self, agent_pos, obs):
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                x, y = agent_pos[0] + dx, agent_pos[1] + dy
+                cell = self.env.unwrapped.grid.get(x, y)
+                if cell and cell.type == 'key':
+                    return True
+        return False
+
+    def is_door_open(self, obs):
+        for x in range(self.env.unwrapped.width):
+            for y in range(self.env.unwrapped.height):
+                cell = self.env.unwrapped.grid.get(x, y)
+                if cell and cell.type == 'door' and cell.is_open:
+                    return True
+        return False
+
+    def at_goal_tile(self, agent_pos, obs):
+        x, y = agent_pos
+        cell = self.env.unwrapped.grid.get(x, y)
+        return cell and cell.type == 'goal'
 
     def get_intrinsic_reward(self, goal, obs):
         return 1.0 if self.is_goal_reached(goal, obs) else 0.0
@@ -140,8 +171,17 @@ class hDQN():
             return torch.IntTensor([random.randrange(self.num_action)])
 
     def is_goal_reached(self, goal, obs):
-        if goal == 6:
-            return np.any(obs["image"][:, :, 0] == 6)  # 是否有绿色箭头格子
+        agent_pos = self.get_agent_position()
+        carrying = obs.get("carrying", None)
+
+        if goal == 0:  # 走到钥匙附近
+            return self.near_key(agent_pos, obs)
+        elif goal == 1:  # 已拿起钥匙
+            return carrying is not None and carrying['type'] == 'key'
+        elif goal == 2:  # 门已打开
+            return self.is_door_open(obs)
+        elif goal == 3:  # 到达目标格子
+            return self.at_goal_tile(agent_pos, obs)
         return False
 
     def update_meta_controller(self, gamma=1.0):
@@ -160,8 +200,8 @@ class hDQN():
         # Compute current Q value, meta_controller takes only state and output value for every state-goal pair
         # We choose Q based on goal chosen.
 
-        # 关键：reshape state_batch 变成 (batch_size, feature_dim)
-        # 这里 feature_dim 就是 fc1 的输入维度，比如 79
+        # reshape state_batch 变成 (batch_size, feature_dim)
+        # feature_dim 就是 fc1 的输入维度，比如 79
         state_batch = state_batch.view(-1, self.meta_controller.fc1.in_features)
         next_state_batch = next_state_batch.view(-1, self.meta_controller.fc1.in_features)
 
